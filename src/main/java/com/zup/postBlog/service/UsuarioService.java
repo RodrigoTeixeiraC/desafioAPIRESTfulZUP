d3abe66342c2f506c115d04eb914902c2b7f7431
RodrigoTeixeiraC <rodrigo.tchagas@gmail.com>
2021-05-04 17:55:14 -0300

    Asustanto UsuarioService

--- src/main/java/com/zup/postBlog/service/UsuarioService.java
+++ src/main/java/com/zup/postBlog/service/UsuarioService.java
@@ -1,8 +1,8 @@
 package com.zup.postBlog.service;
 
-import java.util.List;
+
 import java.util.Optional;
-import java.util.stream.Stream;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
